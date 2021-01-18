18689ed2e6cfd401c8f3586a57b59e52af8db1da
viru2097 <kansaraviraj3@gmail.com>
2021-01-18 23:19:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+    <div className="container-fluid">
+      <Header/>
+      <PricingText/>
+      <PricingCard/>
     </div>
   );
 }
 
-export default App;
+
+function Header(){
+  return(
+    <div className="row bg-light"
+    style={{height="60px",boxShadow:"2px 4px  #dee2e6"}}>
+      <div
+        className
+    </div>
+  )
+}
---
+++ src/Page1.js
@@ -0,0 +1,10 @@
+// export default function Page1(){
+//     return(
+//         <div>
+//             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus
+//       iure impedit veritatis dolorum, quis atque rem temporibus eum in
+//       recusandae autem sunt ut, iusto eligendi quibusdam, nihil cumque
+//       doloribus velit.
+//         </div>
+//     )
+// }
